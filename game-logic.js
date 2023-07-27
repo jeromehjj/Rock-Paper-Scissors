2ecec093780d0a5bd95a8266ff7b9ea2afc07377
jeromehjj <jeromehjj@gmail.com>
2023-07-27 18:20:16 +0800

    Fix bug with score going over 5

--- game-logic.js
+++ game-logic.js
@@ -6,14 +6,15 @@ let result;
 const playerText = document.querySelector('#playerText');
 const computerText = document.querySelector('#computerText');
 const resultText = document.querySelector('#resultText');
-const buttons = document.querySelectorAll('.button')
+const scoreText = document.querySelector('#scoreText');
+const buttons = document.querySelectorAll('.button');
 
 buttons.forEach(button => button.addEventListener("click", () => {
     playerSelection = button.textContent;
     computerSelection = getComputerChoice();
     playerText.textContent = `Player: ${playerSelection}`;
     computerText.textContent = `Computer: ${computerSelection}`;
-    resultText.textContent = playRound(playerSelection, computerSelection);
+    playGame();
 }));
 
 
@@ -24,67 +25,49 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
-        return "It was a draw! " + playerSelection + " is the same as " + computerSelection;
+        resultText.textContent = "It was a draw! " + playerSelection + " is the same as " + computerSelection;
     } else if (playerSelection.toUpperCase() == "ROCK") {
         if (computerSelection.toUpperCase() == "SCISSORS") {
             playerScore++;
-            return "You won! " + playerSelection + " beats " + computerSelection;
+            resultText.textContent = "You won! " + playerSelection + " beats " + computerSelection;
         } else {
             computerScore++;
-            return "You lost! " + computerSelection + " beats " + playerSelection;
+            resultText.textContent = "You lost! " + computerSelection + " beats " + playerSelection;
         }
     } else if (playerSelection.toUpperCase() == "PAPER") {
         if (computerSelection.toUpperCase() == "ROCK") {
             playerScore++;
-            return "You won! " + playerSelection + " beats " + computerSelection;
+            resultText.textContent = "You won! " + playerSelection + " beats " + computerSelection;
         } else {
             computerScore++;
-            return "You lost! " + computerSelection + " beats " + playerSelection;
+            resultText.textContent = "You lost! " + computerSelection + " beats " + playerSelection;
         }
     } else if (playerSelection.toUpperCase() == "SCISSORS") {
         if (computerSelection.toUpperCase() == "PAPER") {
             playerScore++;
-            return "You won! " + playerSelection + " beats " + computerSelection;
+            resultText.textContent = "You won! " + playerSelection + " beats " + computerSelection;
         } else {
             computerScore++;
-            return "You lost! " + computerSelection + " beats " + playerSelection;
+            resultText.textContent = "You lost! " + computerSelection + " beats " + playerSelection;
         }
-    } else {
-        return "Invalid input! choose either Rock, Paper or Scissors"
     }
 }
 
+function playGame() {
+    playRound(playerSelection, computerSelection);
+    scoreText.textContent = `Current Score: Player: ${playerScore} Computer: ${computerScore}`
+    if (playerScore === 5) {
+        scoreText.textContent = "Congrats you won! Player " + playerScore + " : Computer " + computerScore;
+        resetScore();
+    } else if (computerScore === 5) {
+       scoreText.textContent = "You lost! Try again. Player " + playerScore + " : Computer " + computerScore;
+       resetScore();
+    } else {
+        scoreText.textContent = `Current Score: Player: ${playerScore} Computer: ${computerScore}`
+    }
+}
 
-
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-
-//     for (let i = 0; i < 5; i++) {
-
-//         const prompt=require("prompt-sync")({sigint:true});
-//         let playerSelection = prompt("Choose between Rock, Paper or Scissors: ");
-//         computerSelection = getComputerChoice();
-
-//         let result = playRound(playerSelection, computerSelection);
-//         console.log(result);
-
-//         if (result.includes("won")) {
-//             playerScore++;
-//         } else if (result.includes("lost")) {
-//             computerScore++;
-//         } else {
-//             continue;
-//         }
-//     }
-
-//     if (playerScore > computerScore) {
-//         console.log("Congrats you won! Player " + playerScore + " : Computer " + computerScore);
-//     } else if (playerScore < computerScore) {
-//         console.log("You lost! Try again. Player " + playerScore + " : Computer " + computerScore);
-//     } else {
-//         console.log("You somehow drew! Try again. Player " + playerScore + " : Computer " + computerScore);
-//     }
-// }
-
-// game();
+function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+}
